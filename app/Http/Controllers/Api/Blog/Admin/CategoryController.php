3a35383a2586109e231be419a4dc6314ab30c378
Diana Pinchuk <diana.pinchuk@oa.edu.ua>
2026-05-30 21:09:27 +0000

    lab 6

--- app/Http/Controllers/Api/Blog/Admin/CategoryController.php
+++ app/Http/Controllers/Api/Blog/Admin/CategoryController.php
@@ -6,6 +6,8 @@ namespace App\Http\Controllers\Api\Blog\Admin;
 use Illuminate\Http\Request;
 use App\Models\BlogCategory;
 use Illuminate\Support\Str;
+use App\Http\Requests\BlogCategoryUpdateRequest;
+use App\Http\Requests\BlogCategoryCreateRequest;
 
 class CategoryController extends BaseController
 {
@@ -15,32 +17,36 @@ class CategoryController extends BaseController
         return $paginator;
     }
 
-    public function store(Request $request)
+    public function store(BlogCategoryCreateRequest $request) // Використовуємо BlogCategoryCreateRequest
     {
-        $data = $request->all();
+        $data = $request->input();
+
         if (empty($data['slug'])) {
-            $data['slug'] = Str::slug($data['title']);
+            $data['slug'] = \Illuminate\Support\Str::slug($data['title']);
         }
 
-        $item = BlogCategory::create($data);
+        // Створюємо об'єкт через модель
+        $item = (new \App\Models\BlogCategory())->create($data);
 
         if ($item) {
-            return ['success' => true, 'message' => 'Успішно створено'];
+            return ['success' => true, 'message' => 'Успішно збережено'];
         } else {
             return ['message' => 'Помилка збереження'];
         }
     }
 
-    public function update(Request $request, string $id)
+    public function update(BlogCategoryUpdateRequest $request, $id) // Замінили клас реквесту
     {
         $item = BlogCategory::find($id);
+
         if (empty($item)) {
             return ['message' => "Запис id=[{$id}] не знайдено"];
         }
 
         $data = $request->all();
+
         if (empty($data['slug'])) {
-            $data['slug'] = Str::slug($data['title']);
+            $data['slug'] = \Illuminate\Support\Str::slug($data['title']);
         }
 
         $result = $item->update($data);
