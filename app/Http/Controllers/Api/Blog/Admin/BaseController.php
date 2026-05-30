6e78ef0d4c256b53fb5b131aefe525932c03bd73
Diana Pinchuk <diana.pinchuk@oa.edu.ua>
2026-05-30 19:04:18 +0000

    lab 4

---
+++ app/Http/Controllers/Api/Blog/Admin/BaseController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\Api\Blog\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Blog\BaseController as GuestBaseController;
+
+abstract class BaseController extends GuestBaseController
+{
+
+}
