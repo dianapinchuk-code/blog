9d0a2b2a430f3249b0f56f71e5bd33d43940465c
Diana Pinchuk <diana.pinchuk@oa.edu.ua>
2026-05-30 20:07:32 +0000

    lab 5

---
+++ app/Http/Controllers/DiggingDeeperController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\BlogPost;
+use Carbon\Carbon;
+
+class DiggingDeeperController extends Controller
+{
+    public function collections()
+    {
+        $result = [];
+
+        /**
+         * @var \Illuminate\Database\Eloquent\Collection $eloquentCollection
+         */
+        $eloquentCollection = BlogPost::withTrashed()->get();
+
+        // ТОЧКА 1: Перевірка
+       //  dd(__METHOD__, $eloquentCollection, $eloquentCollection->toArray());
+
+        /**
+         * @var \Illuminate\Support\Collection $collection
+         */
+        $collection = collect($eloquentCollection->toArray());
+
+        // ТОЧКА 2: Перевірка
+        /* dd(
+            get_class($eloquentCollection),
+            get_class($collection),
+            $collection
+        );*/
+
+
+        $result['first'] = $collection->first();
+        $result['last'] = $collection->last();
+
+        $result['where']['data'] = $collection
+            ->where('category_id', 10)
+            ->values()
+            ->keyBy('id');
+
+        $result['where']['count'] = $result['where']['data']->count();
+        $result['where']['isEmpty'] = $result['where']['data']->isEmpty();
+        $result['where']['isNotEmpty'] = $result['where']['data']->isNotEmpty();
+
+        $result['where_first'] = $collection
+            ->firstWhere('created_at', '>' , '2020-02-24 03:46:16');
+
+        $result['map']['all'] = $collection->map(function ($item) {
+            $newItem = new \stdClass();
+            $newItem->item_id = $item['id'];
+            $newItem->item_name = $item['title'];
+            $newItem->exists = is_null($item['deleted_at']);
+
+            return $newItem;
+        });
+
+        $result['map']['not_exists'] = $result['map']['all']->where('exists', '=', false)->values()->keyBy('item_id');
+
+        // ТОЧКА 3: Перевірка, щоб побачити результат фільтрації та мапінгу
+        // dd ($result);
+
+        $collection->transform(function ($item) {
+            $newItem = new \stdClass();
+            $newItem->item_id = $item['id'];
+            $newItem->item_name = $item['title'];
+            $newItem->exists = is_null($item['deleted_at']);
+            $newItem->created_at = Carbon::parse($item['created_at']);
+
+            return $newItem;
+        });
+
+        // ТОЧКА 4: Перевірка, як змінилася сама колекція після transform
+        // dd ($collection);
+
+        $newItem = new \stdClass;
+        $newItem->id = 9999;
+
+        $newItem2 = new \stdClass;
+        $newItem2->id = 8888;
+
+        $newItemFirst = $collection->prepend($newItem)->first();
+        $newItemLast = $collection->push($newItem2)->last();
+        $pulledItem = $collection->pull(1);
+
+        // ТОЧКА 5: Перевірка та вилучення елементів
+        // dd(compact('collection', 'newItemFirst' , 'newItemLast', 'pulledItem'));
+
+        $filtered = $collection->filter(function ($item) {
+            if (empty($item->created_at) || !($item->created_at instanceof Carbon)) {
+                return false;
+            }
+
+            $byDay = $item->created_at->isFriday();
+            $byDate = $item->created_at->day == 11;
+
+            return $byDay && $byDate;
+        });
+        // ТОЧКА 6: Перевірка фільтрацію по датах (Carbon)
+        // dd(compact('filtered'));
+
+        $sortedSimpleCollection = collect([5, 3, 1, 2, 4])->sort()->values();
+        $sortedAscCollection = $collection->sortBy('created_at');
+        $sortedDescCollection = $collection->sortByDesc('item_id');
+
+        // ТОЧКА 7: Фінальна перевірка сортування
+         dd(compact('sortedSimpleCollection', 'sortedAscCollection', 'sortedDescCollection'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\RestTestController;
+use App\Http\Controllers\DiggingDeeperController;
+
 
 Route::apiResource('rest', RestTestController::class)->names('restTest');
 Route::get('/', [BlogController::class, 'index']);
@@ -19,4 +21,8 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+Route::group(['prefix' => 'digging_deeper'], function () {
+    Route::get('collections', [DiggingDeeperController::class, 'collections'])
+        ->name('digging_deeper.collections');
 });
